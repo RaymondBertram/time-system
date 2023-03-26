141128fceeb9247cc46676deb0ffb544f2503175
Raymond Bertram <raymondbertram@localRay.fritz.box>
2023-03-26 19:34:47 +0200

    functioning project

--- src/screens/time-registration/time-registration.tsx
+++ src/screens/time-registration/time-registration.tsx
@@ -11,24 +11,41 @@ import "./time-registration.css";
 
 export const TimeRegistration = () => {
   const navigate = useNavigate();
-  const [startDate, setStartDate] = useState<Date>(new Date());
-  const [startTime, setStartTime] = useState(null!);
-  const [endTime, setEndTime] = useState(null!);
+  const [workDate, setWorkDate] = useState<Date>(new Date());
+  const [startTime, setStartTime] = useState('');
+  const [endTime, setEndTime] = useState('');
+
+  const year = dayjs(workDate).year();
+  const month = dayjs(workDate).month();
+  const day = dayjs(workDate).date();
 
   const handleClickNext = () => {
-    if ((startTime && endTime) === null) {
-      alert('Please Enter Data')
+    if ((startTime && endTime) === '') {
+      alert('Es wurden keine Daten eingegeben. Bitte geben Sie die Daten an und versuchen Sie es erneut!')
+      console.log('no data'); 
       navigate(ConfigRoutes.TIME_REGISTRATION)
     } else {
-      console.log('notNull')
-      // Startdate for saving
-      const date = dayjs(startDate).format(Dateformat.DEFAULT);
-    // push to mocked and calculate properties
-    navigate(ConfigRoutes.TIME_OVERVIEW)
+      console.log('data!!')
+      const date = dayjs(workDate).format(Dateformat.DEFAULT);
+      const obj = {
+        date: date,
+        start: startTime,
+        end: endTime,
+        ist: calcTimings(endTime).diff(calcTimings(startTime), 'hours'),
+        soll: 8,
+        difference: calcTimings(endTime).diff(calcTimings(startTime), 'hours') - 8,
+      };
+      mockedData.push(obj);
+      navigate(ConfigRoutes.TIME_OVERVIEW);
     }
   };
 
-  // Get input data 
+  const calcTimings = (time: string) => {
+    const hours = time.slice(0, 2);
+    const minutes = time.slice(3, 5);
+    return dayjs(new Date(year, month, day, +hours, +minutes));
+  }
+
   const handleChangeStartDate = (event: any) => {
     setStartTime(event.target.value);
   };
@@ -44,8 +61,8 @@ export const TimeRegistration = () => {
         <div className="datepicker">
           <h2>Datum auswählen</h2>
           <DatePicker
-            value={startDate}
-            onChange={(date: React.SetStateAction<Date>) => setStartDate(date)}
+            value={workDate}
+            onChange={(date: React.SetStateAction<Date>) => setWorkDate(date)}
           />
         </div>
         <div className="time-registration">
